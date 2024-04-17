dea8b33a53fa76989b69ba7782ee5684574575e4
cnachen <cnachencli@live.cn>
2024-04-17 08:41:19 +0800

    fix: won't build on linux

--- core/dl.c
+++ core/dl.c
@@ -8,10 +8,10 @@ int install_extension(struct cpu *cpu)
 {
 	void *handle;
 
-	handle = dlopen("rvi.so", RTLD_GLOBAL | RTLD_LAZY);
+	handle = dlopen("./rvi.so", RTLD_GLOBAL | RTLD_LAZY);
 	((subprog)dlsym(handle, "register_extension"))(cpu);
 
-	handle = dlopen("rv64i.so", RTLD_GLOBAL | RTLD_LAZY);
+	handle = dlopen("./rv64i.so", RTLD_GLOBAL | RTLD_LAZY);
 	((subprog)dlsym(handle, "register_extension"))(cpu);
 	return 0;
 }
